ea43f452de346b2de839d2d896adac57061a459a
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-02-11 21:56:03 +0100

    feat: install i18next, add language files for english and norwegian, and test usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import "./App.css";
+import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 function App() {
+  const {
+    t,
+    i18n: { changeLanguage, language },
+  } = useTranslation();
+  const [currentLanguage, setCurrentLanguage] = useState<string>(language);
+
+  const handleChangeLanguage = () => {
+    const newLanguage = currentLanguage === "en" ? "nb" : "en";
+    setCurrentLanguage(newLanguage);
+    changeLanguage(newLanguage);
+  };
   return (
-    <>
-      <div>My React app</div>
-      <div>Kitespots 2.0</div>
-      <div>Test github</div>
-    </>
+    <div>
+      <h1>Kite spots 2.0</h1>
+      <h3>{t("currentLanguage")}</h3>
+      <button type={"button"} onClick={handleChangeLanguage}>
+        {t("changeLanguage")}
+      </button>
+    </div>
   );
 }
 
