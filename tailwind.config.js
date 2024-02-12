4d51bbaef0dcd4fc91db7e1862869756e124b06a
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-02-12 12:06:40 +0100

    feat: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,20 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        headerColor: "#2d728f",
+        headerButtonsSelected: "#3b8ea5",
+        webPageBodyBackground: "#f5ee9e",
+        webPageContainerBody: "#f49e4c",
+      },
+    },
   },
   plugins: [],
 };
