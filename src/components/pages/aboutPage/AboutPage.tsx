afa3cdccb0c70f26c6493550d2a3511a656c60eb
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-02-13 13:23:02 +0100

    feat: update pages

--- src/components/pages/aboutPage/AboutPage.tsx
+++ src/components/pages/aboutPage/AboutPage.tsx
@@ -1,3 +1,49 @@
+import { Trans, useTranslation } from "react-i18next";
+
 export const AboutPage = () => {
-  return <div className="w-full">This is the about page</div>;
+  const { t } = useTranslation();
+
+  const kitespotsGithubRepo =
+    "https://github.com/henrikgb/kitespots-react-ui-vite";
+  const linkMeteomatics =
+    "https://www.meteomatics.com/en/weather-api/?msclkid=85e0b029dcb111d13bc7d5e280cfcaa6&utm_source=bing&utm_medium=cpc&utm_campaign=Weather%20API%20(englisch%20ausser%20USA)&utm_term=meteomatics&utm_content=Weather%20Api";
+  const linkStavangerKiteklubb = "https://www.stavangerkiteklubb.com/";
+
+  return (
+    <div>
+      <p className={"font-bold text-2xl mb-4"}>{t("aboutPageHeader")}</p>
+      <div className="mb-4">
+        <Trans i18nKey="aboutPageTextSection1">
+          <a
+            href={`${kitespotsGithubRepo}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`font-bold text-black`}
+          ></a>
+        </Trans>
+      </div>
+      <p className="mb-4">{t("aboutPageTextSection2")}</p>
+      <div className="mb-4">
+        <Trans i18nKey="aboutPageTextSection3">
+          <a
+            href={`${linkMeteomatics}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`font-bold text-black`}
+          ></a>
+        </Trans>
+      </div>
+      <div className="mb-4">
+        <Trans i18nKey="aboutPageTextSection4">
+          <a
+            href={`${linkStavangerKiteklubb}`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`font-bold text-black`}
+          ></a>
+        </Trans>
+      </div>
+      <p className="mb-4">{t("aboutPageTextSection5")}</p>
+    </div>
+  );
 };
--- src/components/pages/contactPage/ContactPage.tsx
+++ src/components/pages/contactPage/ContactPage.tsx
@@ -1,3 +1,23 @@
+import { Trans, useTranslation } from "react-i18next";
+
 export const ContactPage = () => {
-  return <div>This is the contact page</div>;
+  const { t } = useTranslation();
+  const email = "henrik.busengdal@kitespots.no";
+
+  return (
+    <div className={`text-container`}>
+      <p className="font-bold text-2xl mb-4">{t("contactInfoHeader")}</p>
+      <div className="mb-4">
+        <Trans
+          i18nKey="contactInfoMessagePart1"
+          components={{ br: <br /> }}
+        ></Trans>
+      </div>
+      <Trans i18nKey="contactInfoEmail" values={{ email }}>
+        <a href={`mailto:${email}`} className={`font-bold text-black`}></a>
+      </Trans>
+      <br />
+      <p>{t("contactInfoMessagePart2")}</p>
+    </div>
+  );
 };
