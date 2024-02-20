5bd7e72785d00d9c2460cf3f681231c3a27d253b
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-02-20 09:25:52 +0100

    feat: update ContactPage.tsx

--- src/components/pages/contactPage/ContactPage.tsx
+++ src/components/pages/contactPage/ContactPage.tsx
@@ -1,23 +1,28 @@
 import { Trans, useTranslation } from "react-i18next";
+import { Paper } from "../../Paper.tsx";
 
 export const ContactPage = () => {
   const { t } = useTranslation();
   const email = "henrik.busengdal@kitespots.no";
 
   return (
-    <div className={`text-container`}>
-      <p className="font-bold text-2xl mb-4">{t("contactInfoHeader")}</p>
-      <div className="mb-4">
-        <Trans
-          i18nKey="contactInfoMessagePart1"
-          components={{ br: <br /> }}
-        ></Trans>
-      </div>
-      <Trans i18nKey="contactInfoEmail" values={{ email }}>
-        <a href={`mailto:${email}`} className={`font-bold text-black`}></a>
-      </Trans>
-      <br />
-      <p>{t("contactInfoMessagePart2")}</p>
+    <div className="w-full flex justify-center">
+      <Paper>
+        <div className={`text-container`}>
+          <h2>{t("contactInfoHeader")}</h2>
+          <div className="mb-4">
+            <Trans
+              i18nKey="contactInfoMessagePart1"
+              components={{ br: <br /> }}
+            ></Trans>
+          </div>
+          <Trans i18nKey="contactInfoEmail" values={{ email }}>
+            <a href={`mailto:${email}`} className={`font-bold text-black`}></a>
+          </Trans>
+          <br />
+          <p>{t("contactInfoMessagePart2")}</p>
+        </div>
+      </Paper>
     </div>
   );
 };
