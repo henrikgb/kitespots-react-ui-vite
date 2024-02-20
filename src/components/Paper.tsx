d8d1c138d3fec01bffd810d64e5e18cace156b2d
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-02-20 09:26:35 +0100

    feat: update Paper.tsx

--- src/components/Paper.tsx
+++ src/components/Paper.tsx
@@ -3,11 +3,18 @@ import { type ReactNode } from "react";
 interface PaperProps {
   padding?: 0 | 8 | 20;
   children: ReactNode;
+  width?: "w-fit" | "w-full";
+  backgroundColor?: "bg-webPageContainerBody" | "bg-webPageBodyBackground";
 }
-export const Paper = ({ padding = 20, children }: PaperProps) => {
+export const Paper = ({
+  padding = 20,
+  children,
+  width = "w-fit",
+  backgroundColor = "bg-webPageContainerBody",
+}: PaperProps) => {
   return (
     <div
-      className="w-fit border-b-gray-800 shadow-2xl bg-webPageContainerBody"
+      className={`${width} border-b-gray-800 shadow-2xl ${backgroundColor}`}
       style={{ padding: padding }}
     >
       {children}
