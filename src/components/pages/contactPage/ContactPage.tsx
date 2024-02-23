34bdc61c0f057ff075806a527101411610f4870a
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2024-02-24 00:51:12 +0100

    feat: Update contact page and add a new image to assets folder

--- src/components/pages/contactPage/ContactPage.tsx
+++ src/components/pages/contactPage/ContactPage.tsx
@@ -1,13 +1,26 @@
 import { Trans, useTranslation } from "react-i18next";
-import { Paper } from "../../Paper.tsx";
+import { Card, CardBody, CardHeader } from "@material-tailwind/react";
+import sunset from "../../../assets/beachSunset.jpg";
 
 export const ContactPage = () => {
   const { t } = useTranslation();
   const email = "henrik.busengdal@kitespots.no";
 
   return (
-    <div className="w-full flex justify-center">
-      <Paper>
+    <Card className="mt-6 w-full h-full bg-webPageContainerBody">
+      <CardHeader
+        color="blue-gray"
+        className="flex h-full justify-center items-center"
+        style={{ height: 300 }}
+      >
+        <img
+          src={sunset}
+          alt="kitespots-logo"
+          className="w-full h-full object-cover"
+          style={{ height: "100%", width: "100%" }}
+        />
+      </CardHeader>
+      <CardBody className="flex h-[250px] justify-center">
         <div className={`text-container`}>
           <h2>{t("contactInfoHeader")}</h2>
           <div className="mb-4">
@@ -22,7 +35,7 @@ export const ContactPage = () => {
           <br />
           <p>{t("contactInfoMessagePart2")}</p>
         </div>
-      </Paper>
-    </div>
+      </CardBody>
+    </Card>
   );
 };
